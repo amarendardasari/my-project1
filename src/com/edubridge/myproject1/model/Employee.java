2be6b5e49e0ebcd800f93e950dfa12ce17f36f82
amarendardasari <amarendardasari2612@gmail.com>
2024-06-06 11:06:40 +0530

    Employee updated

--- src/com/edubridge/myproject1/model/Employee.java
+++ src/com/edubridge/myproject1/model/Employee.java
@@ -4,6 +4,24 @@ public class Employee {
 	private int employeeId;
 	private String employeeName;
 	private long emloyeeMobile;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public long getEmloyeeMobile() {
+		return emloyeeMobile;
+	}
+	public void setEmloyeeMobile(long emloyeeMobile) {
+		this.emloyeeMobile = emloyeeMobile;
+	}
 	
 
 }
